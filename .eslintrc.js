e97558044d06648f870113a670dca9ecf2d63125
Peter Holczbauer <peter.holczbauer@habyt.com>
2023-03-11 10:34:05 +0100

    fix eslint / prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
-	extends: ["eslint-config-codely/typescript"],
+	"plugins": [
+		"@typescript-eslint",
+		"unused-imports",
+	  ],
+	  "extends": [
+		"plugin:@typescript-eslint/recommended",
+		"prettier"
+	  ],
 	overrides: [
 		{
-			files: ["*.ts", "*.tsx"],
+			files: ['*.ts', '*.tsx'],
 			parserOptions: {
-				project: ["./tsconfig.json"],
+				project: ['./tsconfig.json'],
 			},
 		},
 	],
-};
+}
